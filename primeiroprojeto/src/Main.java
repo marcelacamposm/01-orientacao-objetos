b3dc4374e90e5afc998baab0e038bbfbb169b487
marcelacamposm <marcelacamposm2@gmail.com>
2025-05-27 15:23:10 -0300

    git add

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int yearOfRelease = 2022;
+        System.out.println("Ano de lançamento: " + yearOfRelease);
+        boolean incluidoNoPlano = true;
+
+        //Média calculada entre 3 notas.
+        double notaDoFilme = 8.5;
+
+        double media = (9.8 + 6.3 + 8) /3;
+        System.out.println("Média: " + media);
+        String sinopse;
+        sinopse = """
+        Filme Top Gun
+        Filme de aventura com galã dos anos 80
+        Muito bom!
+        Ano de Lançamento
+        """ + yearOfRelease;
+
+        System.out.println(sinopse);
+
+        // código omitido
+
+        int classificacao = (int) (media/ 2);
+        System.out.println(classificacao);
+
     }
 }
--- segundoprojeto/src/Main.java
+++ segundoprojeto/src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+
     }
+
 }
