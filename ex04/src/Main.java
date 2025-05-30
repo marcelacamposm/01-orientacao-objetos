afbb6909fcd489d8b5949b083425dd0c05a88ebe
marcelacamposm <marcelacamposm2@gmail.com>
2025-05-30 17:40:03 -0300

    git add

--- ex04/src/Main.java
+++ ex04/src/Main.java
@@ -18,15 +18,13 @@ public class Main {
     """);
 
 
-
-
         int opcao = scanner.nextInt();
 
         switch (opcao){
 
-            case 1:
+//Crie um programa que solicite ao usuário digitar um número. Se o número for positivo, exiba "Número positivo", caso contrário, exiba "Número negativo".
 
-    //Crie um programa que solicite ao usuário digitar um número. Se o número for positivo, exiba "Número positivo", caso contrário, exiba "Número negativo".
+            case 1:
 
         System.out.println("Digite um número:");
         int numero = scanner.nextInt();
@@ -43,7 +41,7 @@ public class Main {
         //Peça ao usuário para inserir dois números inteiros. Compare os números e imprima uma mensagem indicando se são
         // iguais, diferentes, o primeiro é maior ou o segundo é maior.
 
-        System.out.println("Agora, digite dois números inteiros:");
+        System.out.println("Digite dois números inteiros:");
         int numero1 = scanner.nextInt();
         int numero2 = scanner.nextInt();
 
@@ -75,12 +73,53 @@ public class Main {
                     case 2:
                         System.out.println("Digite o raio do círculo:");
                         double raioCirculo = scanner.nextDouble();
+                        double areaCirculo = (raioCirculo * raioCirculo) * 3.14;
+                        System.out.println("A área do círculo é: " + areaCirculo);
                 }
 
+               //Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de 1 a 10.
+                case 4:
 
+                    System.out.println("Digite o npumero:");
+                    int numeroTabuada = scanner.nextInt();
+
+                    for (int i = 1; i <= 10; i++) {
+                         System.out.println(numeroTabuada + " x " + i + " = " + (numeroTabuada * i));
+                     }
+
+                    //Crie um programa que solicite ao usuário a entrada de um número inteiro. Verifique se o número é par ou ímpar e exiba uma mensagem correspondente.
+
+                case 5:
+
+                    System.out.println("Digite um número inteiro:");
+                    int numeroP = scanner.nextInt();
+                    if (numeroP % 2 == 0)
+                        System.out.println("O número é par!");
+                     else
+                        System.out.println("O número é ímpar!");
+
+
+                     //Crie um programa que solicite ao usuário um número e calcule o fatorial desse número.
+
+                case 6:
+
+                    System.out.println("Digite um número:");
+
+                    int numeroF = scanner.nextInt();
+
+                    int fatorial = 1;
+                    for (int i = 1; i <= numeroF; i++) {
+                        fatorial *= i;
+                    }
+
+                    System.out.println("O fatorial de " + numeroF + " é " + fatorial);
+                    break;
 
 
 
         }
+
+
+
     }
 }
